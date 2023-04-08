8d59f3c149e466521bca04ac46c006b3b5f98f18
wajima <goo.hiro.w@gmail.com>
2023-04-08 19:26:28 +0900

    todoitem

---
+++ cs_api/Controllers/TodoController.cs
@@ -0,0 +1,29 @@
+﻿using cs_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cs_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private static List<TodoItem> items = new List<TodoItem>()
+        {
+            new TodoItem { Id = 1, Name = @"ミントティーを飲む", IsDone = false, },
+            new TodoItem { Id = 2, Name = @"紅茶を飲む", IsDone = true, },
+            new TodoItem { Id = 3, Name = @"ハーブティーを飲む", IsDone = false, },
+        };
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll() => items;
+
+        [HttpGet("{id}", Name = "Todo")]
+        public ActionResult<TodoItem> GetById(int id)
+        {
+            var item = items.Find(i => i.Id == id);
+            if (item == null) return NotFound();
+            return item;
+        }
+    }
+}
---
+++ cs_api/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace cs_api.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
